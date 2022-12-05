6a33fd57366ae906766713988053ddbdbdde874c
Vellarasan Dhanapathy <vdhanapathy@ntst.com>
2022-12-05 07:33:22 +0530

    Added simple controller and record

--- src/main/java/io/vels/springboot3basics/SpringBoot3BasicsApplication.java
+++ src/main/java/io/vels/springboot3basics/SpringBoot3BasicsApplication.java
@@ -2,6 +2,9 @@ package io.vels.springboot3basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBoot3BasicsApplication {
@@ -10,4 +13,15 @@ public class SpringBoot3BasicsApplication {
         SpringApplication.run(SpringBoot3BasicsApplication.class, args);
     }
 
+}
+
+@RestController
+class StudentController {
+    @GetMapping("/hello")
+    public ResponseEntity<Student> getDetails() {
+        return ResponseEntity.ok(new Student(1, "vellarasan"));
+    }
+}
+
+record Student(int i, String name) {
 }
