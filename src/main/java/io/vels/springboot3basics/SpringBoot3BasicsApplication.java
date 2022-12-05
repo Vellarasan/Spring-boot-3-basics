5c8b9ab1f14ef6d9df77c2d0bec3887f7288bac9
Vellarasan Dhanapathy <vdhanapathy@ntst.com>
2022-12-05 07:46:37 +0530

    Added Exception handling using ProblemDetails

--- src/main/java/io/vels/springboot3basics/SpringBoot3BasicsApplication.java
+++ src/main/java/io/vels/springboot3basics/SpringBoot3BasicsApplication.java
@@ -2,8 +2,15 @@ package io.vels.springboot3basics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,9 +24,22 @@ public class SpringBoot3BasicsApplication {
 
 @RestController
 class StudentController {
-    @GetMapping("/hello")
-    public ResponseEntity<Student> getDetails() {
-        return ResponseEntity.ok(new Student(1, "vellarasan"));
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getDetails(@PathVariable String id) {
+        if (id.equals("empty")) {
+            throw new IllegalArgumentException("Input is invalid");
+        }
+        return ResponseEntity.ok(new Student(1, id));
+    }
+}
+
+@ControllerAdvice
+class StudentControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail onException(IllegalArgumentException iae) {
+        return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), iae.getMessage());
     }
 }
 
